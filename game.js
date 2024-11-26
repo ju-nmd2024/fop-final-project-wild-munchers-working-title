541a4ea5e5cae0a14d40ecf8d1eeb92ce36679b3
Natasha <rike23fw@student.ju.se>
2024-11-26 14:35:01 +0100

    Update game.js
    Game states

--- game.js
+++ game.js
@@ -1,7 +1,39 @@
+let red;
+
+let gameState = 'start';
+
+
 function setup() {
   createCanvas(800, 600);
+  red = color(255, 55, 31);
 }
 
 function draw() {
+  background(255, 140, 0);
+  if (gameState === 'start') {
+    startScreen();
+  } else if (gameState === "game") {
+    gameScreen();
+  } else if (gameState === "result") {
+    resultScreen();
+  }
+}
+
+function startScreen() {
+  background(0);
+
+  //Start button
+  stroke(250);
+  strokeWeight(3);
+  fill(red);
+  textSize(40);
+  text('HERE', 412, 330);
+}
+
+function gameScreen() {
+  background(255, 140, 100);
+}
+
+function resultScreen() {
   background(255, 140, 0);
 }
