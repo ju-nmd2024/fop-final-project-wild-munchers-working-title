cb5b7461ad69d6e69cfecbe87743f1d698ee0960
Natasha <rike23fw@student.ju.se>
2024-12-01 21:30:56 +0100

    Update game.js
    muncherball bugs are fixed, and now the paddel moves

--- game.js
+++ game.js
@@ -1,9 +1,12 @@
 let red;
-let gameState = 'game';
+let gameState = 'start';
 
 let x = 100;
 let y = 100;
 let r = 100;
+
+const foods = ["🌭","🍔","🍞","🥧"];
+let score = 0;
 
 // Paddle variables
 let paddleX = 350;
@@ -16,8 +19,8 @@ let paddleSpeed = 2;
 // Ball position variables
 let ballAcceleration = 0.2;
 let ballVelocityY = 0.2;
-let muncherX = 150;
-let muncherY = 200;
+let muncherX = 400;
+let muncherY = 550;
 let rotation = 0;
 
 function setup() {
@@ -26,78 +29,95 @@ function setup() {
 }
 
 
-function muncherBall(x,y,r){
-translate(x,y);
-rotate(r);
 
-  //main body part
+function muncherBall(x, y, r) {
   push();
-  fill(240,190,0);
-  ellipse(0,0,20);
+  translate(x, y);
+  rotate(r);
+
+  // Main body part
+  push();
+  stroke(0);
+  strokeWeight(1);
+  fill(240, 190, 0);
+  ellipse(0, 0, 20);
   pop();
 
-  //teeth
+  // Teeth
   push();
+  stroke(0); 
+  strokeWeight(0.5); 
   fill(255);
   beginShape();
-  vertex(-9,-4);
-  bezierVertex(-5,-12,5,-12,9,-4);
+  vertex(-9, -4);
+  bezierVertex(-5, -12, 5, -12, 9, -4);
   endShape();
+  line(-9, -4, 9, -4);
+  line(-7, -4, -4, -9);
+  line(-3, -4, -4, -9);
+  line(-3, -4, -1, -9);
+  line(2, -4, 0, -9);
+  line(2, -4, 4, -9);
+  line(7, -4, 4, -9);
   pop();
-  line(-9,-4,9,-4);
-  line(-7,-4,-4,-9);
-  line(-3,-4,-4,-9);
-  line(-3,-4,-1,-9);
-  line(2,-4,0,-9);
-  line(2,-4,4,-9);
-  line(7,-4,4,-9);
 
-  //antenna
+  // Antenna
   push();
-  fill(240,190,0);
+  stroke(0);
+  strokeWeight(1); 
+  fill(240, 190, 0);
   beginShape();
-  vertex(-2,6);
-  bezierVertex(-1,20,1,20,2,6);
+  vertex(-2, 6);
+  bezierVertex(-1, 20, 1, 20, 2, 6);
   endShape();
-  ellipse(0,15,6);
+  ellipse(0, 15, 6);
   pop();
 
-  //eyes
+  // Eyes
+  stroke(0);
+  strokeWeight(0.5);
+  fill(255);
   push();
-  translate(-6,-1);
+  translate(-6, -1);
   rotate(0.6);
-  ellipse(0,0,6,10);
+  ellipse(0, 0, 6, 10);
   pop();
 
   push();
-  translate(6,-1);
+  translate(6, -1);
   rotate(-0.6);
-  ellipse(0,0,6,10);
+  ellipse(0, 0, 6, 10);
   pop();
 
-  //irises
+  // Irises
   push();
+  noStroke(); 
   fill(0);
-  translate(-6,-2);
-  ellipse(0,0,3,4);
+  translate(-6, -2);
+  ellipse(0, 0, 3, 4);
   pop();
 
   push();
+  noStroke(); 
   fill(0);
-  translate(6,-2);
-  ellipse(0,0,3,4);
+  translate(6, -2);
+  ellipse(0, 0, 3, 4);
+  pop();
+
   pop();
 }
 
-const foods = ["🌭","🍔","🍞","🥧"];
-
-
 
 function paddle(){
-  fill(255);
+  push();
+  stroke(0);
+  strokeWeight(0.5);
+  fill(50,60,170);
   rect(paddleX, paddleY, paddleWidth, paddleHeight);
+  pop();
+}
 
-  // Paddle collision coordinates
+function pointsCounter(){
 
 }
 
@@ -123,11 +143,11 @@ function startScreen() {
   strokeWeight(3);
   fill(red);
   textSize(40);
-  text('HERE', 412, 330);
+  text('Click anywhere', 412, 330);
 }
 
 function gameScreen() {
-  //background(255, 140, 100);
+  clear(); 
   background(120,120,120);
 
   textSize(40);
@@ -138,8 +158,19 @@ function gameScreen() {
   pop();
   paddle();
 
+  if (keyIsDown(LEFT_ARROW)) {
+    paddleX -= 4; //Moves to the left
+}
+
+  if (keyIsDown(RIGHT_ARROW)) {
+  paddleX += 4; //Moves to the left
+}
+
+  paddleX = constrain(paddleX, 0, 705);
+
   // Ball's speed
   ballVelocityY = ballVelocityY + ballAcceleration;
+
 
 }
 
@@ -147,11 +178,7 @@ function resultScreen() {
   background(255, 140, 0);
 }
 
-
-
-
-/*
 function mousePressed() {
   gameState = 'game';
 }
-  */
+
